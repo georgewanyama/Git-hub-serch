99bb4da41198bcd29bce34392205089678d4391f
georgewanyama <wanyamageorge680@gmail.com>
2019-11-04 23:44:30 +0300

    create navbar for git hub search app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProfileComponent } from './profile/profile/profile.component';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/profile/profile/profile.component.ts
+++ src/app/profile/profile/profile.component.ts
@@ -1,4 +1,6 @@
+import { ProfileService } from './../profile-service/profile.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-profile',
@@ -6,8 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+ 
+profile:any;
+repos:any;
+username:string;
 
-  constructor() { }
+
+  constructor(private profileService:ProfileService) {
+    
+   }
+
+   findProfile(){
+     this.profileService.updateProfile(this.username)
+     this.profileService.getInfo().subscribe(profile =>{
+      
+      this.profile=profile;
+    }); 
+
+    this.profileService.getRepos().subscribe(repos =>{
+      
+      this.repos=repos;
+    })
+   }
 
   ngOnInit() {
   }
