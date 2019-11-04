4c10aff5e7732e17807315b72200f8d10dbaf8b6
georgewanyama <wanyamageorge680@gmail.com>
2019-11-04 22:55:29 +0300

    my first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProfileComponent } from './profile/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
